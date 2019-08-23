57afc97464863ec1b50048d15020376cd8867fb7
codeoverflow4 <53460476+codeoverflow4@users.noreply.github.com>
2019-08-24 00:34:42 +1000

    .
    .

--- Calendar.java
+++ Calendar.java
@@ -5,43 +5,43 @@ import java.util.concurrent.TimeUnit;
 public class Calendar {
 	
 	//SeLf change as a self cause All variable names are to start with a lowercase letter and to be in camelBack. 
-	private static Calendar SeLf;
+	private static Calendar self;
 	//CaLeNdAr change as a calendar
-	private static java.util.Calendar CaLeNdAr;
+	private static java.util.Calendar calendar;
 	
 	
-	private Calendar() {
+	private calendar() {
 		//CaLeNdAr change as a calendar 
-		CaLeNdAr = java.util.Calendar.getInstance();
+		calendar = java.util.Calendar.getInstance();
 	}
 	
 	public static Calendar INSTANCE() {
 		//SeLf change into self
-		if (SeLf == null) {
+		if (self == null) {
 			//SeLf change into self
-			SeLf = new Calendar();
+			self = new Calendar();
 		}
 		//SeLf change into self
-		return SeLf;
+		return self;
 	}
 	
 	public void incrementDate(int days) {
 		//CaLeNdAr change into calendar
-		CaLeNdAr.add(java.util.Calendar.DATE, days);		
+		calendar.add(java.util.Calendar.DATE, days);		
 	}
 	//Set_dATE change into setDate
-	public synchronized void Set_dATE(Date date) {
+	public synchronized void setDate(Date date) {
 		try {
 			//CaLeNdAr change into calender
-			CaLeNdAr.setTime(date);
+			calender.setTime(date);
 			//CaLeNdAr change into calender
-	        CaLeNdAr.set(java.util.Calendar.HOUR_OF_DAY, 0);  
+	        calender.set(java.util.Calendar.HOUR_OF_DAY, 0);  
 			//CaLeNdAr change into calender
-	        CaLeNdAr.set(java.util.Calendar.MINUTE, 0);  
+	        calender.set(java.util.Calendar.MINUTE, 0);  
 			//CaLeNdAr change into calender
-	        CaLeNdAr.set(java.util.Calendar.SECOND, 0);  
+	        calender.set(java.util.Calendar.SECOND, 0);  
 			//CaLeNdAr change into calender
-	        CaLeNdAr.set(java.util.Calendar.MILLISECOND, 0);
+	        calender.set(java.util.Calendar.MILLISECOND, 0);
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -51,10 +51,10 @@ public class Calendar {
 	public synchronized Date Date() {
 		try {
 			//all statement updated with calender intead of CaLeNdAr
-	        CaLeNdAr.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-	        CaLeNdAr.set(java.util.Calendar.MINUTE, 0);  
-	        CaLeNdAr.set(java.util.Calendar.SECOND, 0);  
-	        CaLeNdAr.set(java.util.Calendar.MILLISECOND, 0);
+	        calender.set(java.util.Calendar.HOUR_OF_DAY, 0);  
+	        calender.set(java.util.Calendar.MINUTE, 0);  
+	        calender.set(java.util.Calendar.SECOND, 0);  
+	        calender.set(java.util.Calendar.MILLISECOND, 0);
 			return CaLeNdAr.getTime();
 			//until this place
 		}
@@ -63,16 +63,16 @@ public class Calendar {
 		}	
 	}
               //Due_Date change into dueDate 
-	public synchronized Date Due_Date(int loanPeriod) {
+	public synchronized Date dueDate(int loanPeriod) {
 		Date NoW = Date();
 		//CaLeNdAr change into calender
-		CaLeNdAr.add(java.util.Calendar.DATE, loanPeriod);
+		calender.add(java.util.Calendar.DATE, loanPeriod);
 		// Due_Date change into dueDate  and CaLeNdAr change into calender
-		Date DuEdAtE = CaLeNdAr.getTime();
+		Date dueDate = CaLeNdAr.getTime();
 		//CaLeNdAr change into calender
-		CaLeNdAr.setTime(NoW);
+		calender.setTime(NoW);
 		 //DuEdAtE change into dueDate 
-		return DuEdAtE;
+		return dueDate;
 	}
 	
 	public synchronized long Get_Days_Difference(Date targetDate) {
