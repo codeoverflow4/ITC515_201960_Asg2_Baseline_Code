8e2c4e530d0cae333d2571ca7ff7fe47e615d03a
codeoverflow4 <53460476+codeoverflow4@users.noreply.github.com>
2019-08-23 19:12:55 +1000

    Update ReturnBookControl.java
    //Object  name changed as library
    //Object  name changed as currentLoan
    //State name has changed 
    //Class name has changed as BookScanned
    //Class name has changed as ScanningComplete
    //Class name has changed as DischargeLoan

--- ReturnBookControl.java
+++ ReturnBookControl.java
@@ -1,34 +1,35 @@
+// author :RamithaSilva mediator :RushanDevanga Reviewer:OsandaRanawera
 public class ReturnBookControl {
 
 	private ReturnBookUI Ui;
 	private enum CONTROL_STATE { INITIALISED, READY, INSPECTING };
 	private CONTROL_STATE sTaTe;
 	
-	private library lIbRaRy;
-	private loan CurrENT_loan;
+	private library library; //Object  name changed as library
+	private loan currentLoan; //Object  name changed as currentLoan
 	
 
 	public ReturnBookControl() {
-		this.lIbRaRy = lIbRaRy.INSTANCE();
-		sTaTe = CONTROL_STATE.INITIALISED;
+		this.lIbRaRy = lIbRaRy.INSTANCE(); //Object  name changed as library
+		state = CONTROL_STATE.INITIALISED; //State name has changed 
 	}
 	
-	
-	public void Set_UI(ReturnBookUI ui) {
+	//class name changed as SetUi
+	public void SetUi (ReturnBookUI ui) {
 		if (!sTaTe.equals(CONTROL_STATE.INITIALISED)) {
 			throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.Ui = ui;
 		ui.Set_State(ReturnBookUI.UI_STATE.READY);
-		sTaTe = CONTROL_STATE.READY;		
+		State = CONTROL_STATE.READY;	//State name has changed	
 	}
 
-
-	public void Book_scanned(int Book_ID) {
+	//Class name has changed as BookScanned
+	public void BookScanned(int Book_ID) {
 		if (!sTaTe.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
 		}	
-		book CUR_book = lIbRaRy.Book(Book_ID);
+		book CUR_book = library.Book(Book_ID); //library name changed
 		
 		if (CUR_book == null) {
 			Ui.display("Invalid Book Id");
@@ -38,39 +39,39 @@ public class ReturnBookControl {
 			Ui.display("Book has not been borrowed");
 			return;
 		}		
-		CurrENT_loan = lIbRaRy.LOAN_BY_BOOK_ID(Book_ID);	
+		currentLoan = library.LOAN_BY_BOOK_ID(Book_ID);//object names has been changed
 		double Over_Due_Fine = 0.0;
-		if (CurrENT_loan.OVer_Due()) {
-			Over_Due_Fine = lIbRaRy.CalculateOverDueFine(CurrENT_loan);
+		if (currentLoan.Over_Due()) {//object names has been changed as currentLoan and Over_Due
+			Over_Due_Fine = lIbRaRy.CalculateOverDueFine(currentLoan);//parameter name has changed
 		}
 		Ui.display("Inspecting");
 		Ui.display(CUR_book.toString());
-		Ui.display(CurrENT_loan.toString());
+		Ui.display(currentLoan.toString());
 		
-		if (CurrENT_loan.OVer_Due()) {
+		if (currentLoan.Over_Due()) {//object names has been changed as currentLoan and Over_Due
 			Ui.display(String.format("\nOverdue fine : $%.2f", Over_Due_Fine));
 		}
 		Ui.Set_State(ReturnBookUI.UI_STATE.INSPECTING);
-		sTaTe = CONTROL_STATE.INSPECTING;		
+		state = CONTROL_STATE.INSPECTING;//object name has changed as state		
 	}
 
-
-	public void Scanning_Complete() {
+	//Class name has changed as ScanningComplete
+	public void ScanningComplete() {
 		if (!sTaTe.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
 		}	
 		Ui.Set_State(ReturnBookUI.UI_STATE.COMPLETED);		
 	}
 
-
-	public void Discharge_loan(boolean isDamaged) {
+	//Class name has changed as DischargeLoan
+	public void DischargeLoan(boolean isDamaged) {
 		if (!sTaTe.equals(CONTROL_STATE.INSPECTING)) {
 			throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
 		}	
-		lIbRaRy.Discharge_loan(CurrENT_loan, isDamaged);
-		CurrENT_loan = null;
+		library.Discharge_loan(currentLoan, isDamaged);//object and parameter names has changed as library and currentloan
+		currentLoan = null;
 		Ui.Set_State(ReturnBookUI.UI_STATE.READY);
-		sTaTe = CONTROL_STATE.READY;				
+		state = CONTROL_STATE.READY;				
 	}
 
 
